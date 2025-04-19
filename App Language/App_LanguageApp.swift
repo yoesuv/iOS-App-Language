5ef13aba993dc099bce801c543a58367b9c1d6c4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-04-19 13:31:21 +0700

    :tada: initial commit

---
+++ App Language/App_LanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_LanguageApp.swift
+//  App Language
+//
+//  Created by Yusuf Saifudin on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct App_LanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
